7902971229587f0880721b8bb56d73cab70ce9b1
Varnahal <danielmarcelino91@gmail.com>
2023-10-27 19:27:54 -0300

    arrumei a função de login e criei a tela de cadastro

--- teste_crud_login/app_crud_login/urls.py
+++ teste_crud_login/app_crud_login/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("login/", views.loginPage, name="login"),
     path("cadastro/", views.cadastro, name="cadastro"),
     path("verifica/", views.verificaLogin, name="verificaLogin"),
+    path("logout/", views.logout, name="logout"),
     path("CadastraUsuario/", views.CadastraUsuario, name="CadastraUsuario"),
 ]
--- teste_crud_login/app_crud_login/views.py
+++ teste_crud_login/app_crud_login/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import User
 
 # Create your views here.
 def index(request):
-    return render(request,'app_crud_login/index.html')
+    if request.session.has_key('logged'):
+        return render(request,'app_crud_login/index.html',{'logged':True,'user_name':request.session['username']})
+    else:
+        return render(request,'app_crud_login/index.html')
 
 
 def loginPage(request):
     return render(request,'app_crud_login/login.html')
 
+
 def verificaLogin(request):
     """Verifica se o usuario esta cadastrado no sistema"""
     email = request.POST['email']
-    usuario = User.objects.get(pk=1)
-    return render(request,f'usuario == {usuario.name} email == {email}')
+    password = request.POST['password']
+    try:
+        usuario = User.objects.get(email=email,password=password)
+    except (User.DoesNotExist):
+        return render(request,'app_crud_login/login.html',{'error_message' : 'Usuario não encontrado'})
+    request.session['logged'] = True
+    request.session['username'] = usuario.name
+    return HttpResponseRedirect('/',{'logged':True,'user_name':usuario.name})
+
+
+def logout(request):
+    del request.session['logged']
+    return HttpResponseRedirect('/',{'logged':False})
 
 
 def CadastraUsuario(request):
@@ -22,4 +38,5 @@ def CadastraUsuario(request):
 
 
 def cadastro(request):
+    return render(request,'app_crud_login/cadastro.html')
     pass
